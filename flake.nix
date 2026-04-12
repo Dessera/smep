f14b1fb62973a138e805bca51e2d6cea46acfb3c
Dessera <dessera@qq.com>
2026-04-12 15:57:08 +0800

    feat(flake): add nixConfig for extra substituters and trusted public keys

--- flake.nix
+++ flake.nix
@@ -24,6 +24,11 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = [ "https://cache.nixos-cuda.org" ];
+    extra-trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+  };
+
   outputs =
     {
       flake-parts,
